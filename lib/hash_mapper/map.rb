1b9fc1f93c10c87e4123a39a75b986b80f99b003
Rodrigo Pavano <rodrigopavano@gmail.com>
2014-08-07 00:10:40 -0300

    Extracted nested classes into their own files

---
+++ lib/hash_mapper/map.rb
@@ -0,0 +1,37 @@
+module HashMapper
+  class Map
+
+    def initialize(hash=nil, &block)
+      @old_hash = hash || {}
+      @rules = []
+      instance_exec(&block) if block_given?
+    end
+
+    def source=(hash)
+      @old_hash = hash
+    end
+
+    def map_key(old, new, options={})
+      @rules << Rule.new(old, new, options)
+    end
+
+    def key(key, options={})
+      map_key(key, key, options)
+    end
+
+    def scope(key, &block)
+      scope = ScopeRule.new(key, &block)
+      @rules << scope
+      scope.map
+    end
+
+    def to_h
+      {}.tap do |new_hash|
+        @rules.each do |rule|
+          rule.apply(@old_hash, new_hash)
+        end
+      end
+    end
+
+  end
+end
---
+++ lib/hash_mapper/scope_rule.rb
@@ -0,0 +1,22 @@
+module HashMapper
+  class ScopeRule
+
+    def initialize(key, &block)
+      raise ArgumentError unless block_given?
+      @key = key
+      @block = block
+    end
+
+    def map
+      @map ||= Map.new
+    end
+
+    def apply(old, new, options={})
+      map.source = old
+      map.instance_exec(&@block)
+      new[@key] = map.to_h
+    end
+
+  end
+end
+
